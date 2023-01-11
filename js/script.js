9c2641a1ed2a3642d376196756fe6e8e9844e3f3
Rafael Zapico <120668903+RafaelZapicoNiharu@users.noreply.github.com>
2023-01-11 20:56:31 -0300

    tirando anotações

--- js/script.js
+++ js/script.js
@@ -1,73 +1,58 @@
 const form = document.forms[0];
-const exemplosFunc = [ 
-        {
-            "nome": "Maria José",
-            "mesNasc": '10',
-            "diaNasc": '28',
-            "anoNasc": '2003',
-            "numero": "32998363728",
-        }, {
-            "nome": "Tifanny Wilson",
-            "mesNasc": '09',
-            "diaNasc": '03',
-            "anoNasc": '2003',
-            "numero": "3299934888",
-        }, {
-            "nome": "Patricia Almeida",
-            "mesNasc": '10',
-            "diaNasc": '28',
-            "anoNasc": '2003',
-            "numero": "32999934287",
-        }
-    ];
-
-    
-    form.Mensagem.addEventListener("keypress", function(evento) {
-        
-
-      const maximo = 76;
-      tamanhoDigitado = form.Mensagem.value.length;     
-      if(tamanhoDigitado >= maximo) {
-          evento.preventDefault();
-          window.alert("Tamanho máximo atingido");
-
-      }
-     
-        
-    });
- 
-    
-
+const exemplosFunc = [
+    {
+        "nome": "Maria José",
+        "mesNasc": '10',
+        "diaNasc": '28',
+        "anoNasc": '2003',
+        "numero": "32998363728",
+    }, {
+        "nome": "Tifanny Wilson",
+        "mesNasc": '09',
+        "diaNasc": '03',
+        "anoNasc": '2003',
+        "numero": "3299934888",
+    }, {
+        "nome": "Patricia Almeida",
+        "mesNasc": '10',
+        "diaNasc": '28',
+        "anoNasc": '2003',
+        "numero": "32999934287",
+    }
+];
+form.Mensagem.addEventListener("keypress", function (evento) {
+    const maximo = 76;
+    tamanhoDigitado = form.Mensagem.value.length;
+    if (tamanhoDigitado >= maximo) {
+        evento.preventDefault();
+        window.alert("Tamanho máximo atingido");
+    }
+});
 form.addEventListener("submit", (event) => {
-    event.preventDefault();                                 //fazer campo pra inserir a API key 
+    event.preventDefault();                             
     const data = form.dataNascimento.value;
-  console.log(data);
-   const chave = form.apeikey.value;
+    console.log(data);
+    const chave = form.apeikey.value;
     pesquisaFuncionarios(data).forEach(aniversariante => {
         const mensagem = `Cara(o) ${aniversariante.nome}, nos da Niharu desejamos a voce um Feliz Aniversario!`
             + form.Mensagem.value;
         mandarSMS(mensagem, aniversariante.numero, chave);
-  }); 
- 
-
+    });
 })
 function mandarSMS(message, number, chave) {  //função para enviar o sms
-    const APIkey = chave; //insira sua apikey aqui para poder utilizar o serviço
+    const APIkey = chave; 
     const BaseURL = "https://api.nvoip.com.br/v2/sms";
     const URL = BaseURL + "?napikey=" + APIkey;
-
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
-
     const bodyParameters = {
         numberPhone: number,
         message: message,
         flashSms: false
     };
-
     const parameters = { method: 'POST', headers: headers, body: JSON.stringify(bodyParameters) };
     fetch(URL, parameters)
-        .then(function(response){
+        .then(function (response) {
             console.log(response);
             window.alert("Mensagens enviadas!")
         }).catch(function (err) {
@@ -79,16 +64,14 @@ function pesquisaFuncionarios(data) { //acha os funcionarios que fazem aniversar
     const aniversariantes = [];
     const dataparte = data.split('-');
     console.log(dataparte);
-     exemplosFunc.forEach(func => {
+    exemplosFunc.forEach(func => {
 
-        if(func.mesNasc==dataparte[1]){
-            if(func.diaNasc==dataparte[2]){
+        if (func.mesNasc == dataparte[1]) {
+            if (func.diaNasc == dataparte[2]) {
                 aniversariantes.push(func);
             }
         }
-         
-     
-   });
+    });
     console.log(aniversariantes);
     return aniversariantes;
 }
